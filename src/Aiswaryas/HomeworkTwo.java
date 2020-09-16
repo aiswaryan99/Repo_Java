47a741dd41b06d22e08a0871871868144fe8af9a
Aiswarya Nair <naiswarya7373@gmail.com>
2020-09-15 20:16:55 -0400

    First homework

---
+++ src/Aiswaryas/HomeworkTwo.java
@@ -0,0 +1,59 @@
+package Aiswaryas;
+
+public class HomeworkTwo {
+
+    public static void main(String[] args) {
+
+        // 1. Number of hours in a day
+
+        byte noOfHours= 24;
+        System.out.println("There are " + noOfHours + " hours in a day.");
+
+        // 2. Max No. of Days in a Year
+
+        short noOfDays= 365;
+        System.out.println("There are " + noOfDays + " days in a year." );
+
+        // 3. Total Number of employees in an organization
+
+        int totalOfEmployees= 3500;
+        System.out.println("There are " + totalOfEmployees + " employees in the Technosoft Corporation. ");
+
+        // 4. Population in a country
+
+        long populatonOfCountry= 328200000;
+        System.out.println("The population of the United States of America is " + populatonOfCountry + ".");
+
+        // 5. Interest Rate
+
+        double interestRate1= 13.99;
+        double interestRate2= 23.99;
+        System.out.println("The interest rates of American Express Credit Cards ranges from " + interestRate1 + " to " + interestRate2 + ".");
+
+        // 6. Balance in a bank account.
+
+        double accBalance= 12254836.25;
+        float savingsAcc= 15000.23f;
+        System.out.println("Your Current Account Balance :  " + accBalance);
+        System.out.println("Your Savings Account Balance : " + savingsAcc);
+
+        // 7. Does the sun rise from the west? Store Answer
+
+        boolean sunRiseWest= false;
+        boolean sunRiseEast= true;
+        System.out.println("Does the sun rise in the west? " + sunRiseWest);
+        System.out.println("Does the sun rise in the east? " + sunRiseEast);
+
+        //8. Initials of your name
+
+        String nameInitial= "AN";
+        System.out.println("The Initials of my name are " + nameInitial + "." );
+
+        //9. Your Name
+
+        String fullName= "Aiswarya Nair";
+        String nickName= "Ash";
+        System.out.println("My full name is " + fullName + " and you can call me " + nickName + "!" );
+
+    }
+}
